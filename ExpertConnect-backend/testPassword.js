215474f173975a5183501809397eee5358a3ba86
Mohamed Grami <101901960+GramiMohamed@users.noreply.github.com>
2024-12-30 16:50:20 +0100

    Update testPassword.js

--- ExpertConnect-backend/testPassword.js
+++ ExpertConnect-backend/testPassword.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
-const password = '123456'; // The plain-text password you're testing
-const storedHash = '$2b$10$EVrwJh3uGeexbjFeXRjbYOthWf8rYRZljviIZWIhAjn.Znb9lLT8G'; // Your stored hash from DB
+const password = ''; // The plain-text password you're testing
+const storedHash = ''; // Your stored hash from DB
 
 // Use bcrypt.compare to check if the password matches the hash
 bcrypt.compare(password, storedHash, (err, res) => {
